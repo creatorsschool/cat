3917efc22d0fbb700a9e8707c964292d565753eb
Patricia Madureira <patriciamadureira_15@hotmail.com>
2015-07-23 13:03:26 +0100

    adds member list, remove button and search feature

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,25 @@
-get "/" do 
-	erb:index
+
+get "/member" do
+erb :index
 end
 
-get "/member/create" do 
-
-erb :"/member/create" 
+get "/member/list" do
+	@members = Member.all 
+	erb :"member/list"
 end
+
+get "/member/:id/remove" do
+	@member = Member.find(params[:id]).destroy
+redirect "/member/list"
+end
+
+
+get "/member/search" do
+	if params[:name].downcase && params[:name] != ""
+		@members = Member.where("name LIKE ?", "%#{params[:name]}%")
+	 erb :"member/list"
+	else
+		"Sorry, no matches found"
+	end
+end
+
